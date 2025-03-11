153a2cf011a0d801f261bf7f1240d3015de75610
Mohammed Kaif <Kaifm1280@gmail.com>
2025-03-11 14:42:23 +0530

    .

---
+++ Image-Gallery-Js/script.js
@@ -0,0 +1,16 @@
+const scrollContainer = document.querySelector('.gallery-container');
+const leftBtn = document.querySelector('.scroll-left');
+const rightBtn = document.querySelector('.scroll-right');
+
+scrollContainer.addEventListener('wheel', (e) => {
+    e.preventDefault();
+    scrollContainer.scrollLeft -= e.deltaY;
+})
+
+leftBtn.addEventListener('click', () => {
+    scrollContainer.scrollLeft -= 150;
+})
+
+rightBtn.addEventListener('click', () => {
+    scrollContainer.scrollLeft += 150;
+})
