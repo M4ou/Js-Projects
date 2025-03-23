69facba8508ef180ff9b3aa2fb472702d925e50f
Mohammed Kaif <Kaifm1280@gmail.com>
2025-03-23 13:09:33 +0530

    added js  Query

---
+++ Image-Search-Engine/script.js
@@ -0,0 +1,4 @@
+let Search = document.querySelector('#search');
+let SearchBox = document.querySelector('#search-box');
+let SearchResult = document.querySelector('#search-img');
+let ShowMore = document.querySelector('#show-more-btn');
