ad944b76a44f9a01a0b95701cfe8f121b923dfb9
Mohammed Kaif <Kaifm1280@gmail.com>
2025-03-30 14:19:21 +0530

    added smooth animation in sidebar

--- Responsive/script.js
+++ Responsive/script.js
@@ -1,10 +1,17 @@
 function showSidebar() {
     const sidebar = document.querySelector('.side-bar');
-    sidebar.style.display = "flex"
+    sidebar.style.display = "flex";
+    setTimeout(() => {
+        sidebar.style.opacity = "1";
+        sidebar.style.transform = "translateX(0)";
+    }, 100);
 }
 
 function hideSidebar() {
     const sidebar = document.querySelector('.side-bar');
-    sidebar.style.display = "none"
-
+    sidebar.style.opacity = "0";
+    sidebar.style.transform = "translateX(100%)";
+    setTimeout(() => {
+        sidebar.style.display = "none";
+    }, 300);
 }
