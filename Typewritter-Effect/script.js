b3d95bfb632c04124387308287586316a76c193b
Mohammed Kaif <Kaifm1280@gmail.com>
2025-04-08 12:53:25 +0530

    typewritter effect

---
+++ Typewritter-Effect/script.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let typingAnimation = new Typed(".type-animation", {
+        strings: [
+            'Frontend Developer',
+            'UI Designer',
+            'Web Engineer',
+            'JavaScript Expert',
+            'CSS Enthusiast'
+        ],
+        typeSpeed: 100,
+        backSpeed: 50,
+        backDelay: 1000,
+        loop: true,
+        showCursor: true,
+        cursorChar: '|',
+    })
+});
