d20153ad06f1daa5542c83fa7043035e3412652d
Mohammed Kaif <Kaifm1280@gmail.com>
2025-04-10 11:55:33 +0530

    added responsive header

--- Search-Filter/script.js
+++ Search-Filter/script.js
@@ -1,2 +1,18 @@
-document.addEventListener("DOMContentLoaded", function () {
-});
+// Responsive Sidebar
+function showSidebar() {
+    const sidebar = document.querySelector('.side-bar');
+    sidebar.style.display = "flex";
+    setTimeout(() => {
+        sidebar.style.opacity = "1";
+        sidebar.style.transform = "translateX(0)";
+    }, 300)
+};
+
+function hideSidebar() {
+    const sidebar = document.querySelector('.side-bar');
+    sidebar.style.opacity = "0";
+    sidebar.style.transform = "translateX(100%)";
+    setTimeout(() => {
+        sidebar.style.display = "none";
+    }, 300)
+};
